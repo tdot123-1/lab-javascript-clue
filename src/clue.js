406f26542b394d4bbf7d9a26861f347c61904b5f
Thomas Kruithof <tkruithof95@gmail.com>
2024-06-07 17:29:51 +0200

    iteration 2

--- src/clue.js
+++ src/clue.js
@@ -88,23 +88,32 @@ const roomsArray = [
 // Weapons Array
 
 const weaponsArray = [
-    {"name": rope, "weight": 10},
-    {"name": knife, "weight": 8},
-    {"name": candlestick, "weight": 2},
-    {"name": dumbbell, "weight": 30},
-    {"name": poison, "weight": 2},
-    {"name": axe, "weight": 15},
-    {"name": bat, "weight": 13},
-    {"name": trophy, "weight": 25},
-    {"name": pistol, "weight": 20},
+    {"name": "rope", "weight": 10},
+    {"name": "knife", "weight": 8},
+    {"name": "candlestick", "weight": 2},
+    {"name": "dumbbell", "weight": 30},
+    {"name": "poison", "weight": 2},
+    {"name": "axe", "weight": 15},
+    {"name": "bat", "weight": 13},
+    {"name": "trophy", "weight": 25},
+    {"name": "pistol", "weight": 20},
 ];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(cardStack) {
+    const max = cardStack.length;
+    return cardStack[Math.floor(Math.random() * max)];
+}
 
-function pickMystery() {}
+function pickMystery() {
+    const mystery = {}
+    mystery.suspect = selectRandom(suspectsArray);
+    mystery.weapon = selectRandom(weaponsArray);
+    mystery.room = selectRandom(roomsArray);
+    return mystery
+}
 
 
 // ITERATION 3
